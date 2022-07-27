262544f2805a3ad5ca3430ed385269f82232cb31
Orunov Sultan <sultan.orunov@mail.ru>
2022-07-27 22:24:34 +0500

    Добавил в .gitignore папку vendor, удалил токен и добавил описание в README

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ require_once ('vendor/autoload.php');
 
 use \Dejurin\GoogleTranslateForFree;
 
-const TOKEN = '5477052701:AAFJ7IZJWO0uWJnMQA6i7FUmA9YoTqC8Qzk';
+const TOKEN = 'ВВЕДИТЕ ВАШ ТОКЕН';
 const BASE_URL = 'https://api.telegram.org/bot'. TOKEN .'/';
 
 $telegram = new Telegram(TOKEN);
