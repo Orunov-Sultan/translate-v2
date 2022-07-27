a94a892123c7da34a71a31cae944f67a74a4dc87
Orunov Sultan <sultan.orunov@mail.ru>
2022-07-27 20:47:20 +0500

    First commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once ('vendor/autoload.php');
+
+use \Dejurin\GoogleTranslateForFree;
+
+const TOKEN = '5477052701:AAFJ7IZJWO0uWJnMQA6i7FUmA9YoTqC8Qzk';
+const BASE_URL = 'https://api.telegram.org/bot'. TOKEN .'/';
+
